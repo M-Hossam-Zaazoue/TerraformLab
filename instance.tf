f54d54e85885c8c1a0339fd3ce74ec2cc98adee4
Mohamed Hossam Zaazoue <m.hossam.zaazoue@gmail.com>
2022-04-01 11:18:37 +0200

    fix key pair

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "HossamEC2" {
   ami           = var.ami_id
   instance_type = var.ec2_type
   vpc_security_group_ids = [ aws_security_group.HossamSecGrp.id ]
+  key_name = var.public_key_pair
 
   tags = {
     Name = "HossamEC2"
